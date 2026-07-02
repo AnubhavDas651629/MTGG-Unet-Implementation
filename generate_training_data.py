c1a2918624fadeca7f98f1fcafc5f2d905da0ad0
Anubhavdas651 <anubhavdas651@gmail.com>
2026-07-02 12:34:15 +0530

    added validation and training functions

--- generate_training_data.py
+++ generate_training_data.py
@@ -18,3 +18,56 @@ def generate_graph_seq2seq_io_data(
     if add_day_in_week:
         day_in_week = np.zeros(shape=(num_samples, num_nodes, 7))
         day_in_week[np.arrange(num_samples), :, df.index.dayorweeek] = 1
+        data_list.append(day_in_week)
+
+    data = np.concatenate(data_list, axis=-1)
+
+    x, y = [], []
+
+    min_t = abs(min(x_offset))
+    max_t = abs(num_samples - abs(max(y_offsets)))
+
+    for t in range(min_t, max_t):
+        x_t = data[t + x_offset, ...]
+        y_t = data[t + y_offsets, ...]
+        x.append(x_t)
+        y.append(y_t)
+
+    x = np.stack(x, axis = 0)
+    y = np.stack(y, axis=0)
+    return x, y
+
+def generate_train_val_test(args):
+    df = pd.read_hdf(args.traffic_df_filename)
+    x_offsets = np.sort(
+        np.concatenate((np.arrange(-11, 1, 1,)))
+    )
+
+    y_offsets = np.sort(np.arrange(1,13,1))
+
+    x, y = generate_graph_seq2seq_io_data(
+        df,
+        x_offsets=x_offsets,
+        y_offsets=y_offsets,
+        add_time_in_day=True,
+        add_day_in_week=False,
+    )
+    print("x shape", x.shape, ", y shape: ", y.shape)
+
+    num_samples = x.shape[0]
+    num_test = round(num_samples * 0.2)
+    num_train = round(num_samples * 0.7)
+    num_val = num_samples - num_test - num_train
+
+    x_train, y_train = x[:num_train], y[:num_train]
+
+    x_val, y_val = (
+        x[num_train: num_train + num_val],
+        y[num_train: num_train + num_val],
+    )
+
+    x_test, y_test = x[-num_test:], y[-num_test:]
+
+    
+
+
