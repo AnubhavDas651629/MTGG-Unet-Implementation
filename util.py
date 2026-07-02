89a29cc80881bbd1eab4660ab6f12a5af6c01688
Anubhavdas651 <anubhavdas651@gmail.com>
2026-07-02 13:40:35 +0530

    utility function

---
+++ util.py
@@ -0,0 +1,45 @@
+import pickle
+import numpy as np
+import os
+import scipy.sparse as sp
+import torch
+from scipy.sparse import linalg
+from torch.autograd import Variable
+
+def normal_std(x):
+    return x.std() * np.sqrt((len(x) - 1.)/(len(x)))
+
+class DataLoaderS(object):
+    def __init__(self, file_name, train, valid, device, horizon, window, normalize=2):
+        self.P = window
+        self.h = horizon
+        fin = open(file_name)
+        self.rawdat = np.loadtxt(fin, delimiter=',')
+        self.dat = np.zeros(self.rawdat.shape)
+        self.n, self.m = self.dat.shape
+        self.normalize = 2
+        self.scale = np.ones(self.m)
+        self._normalized(normalize)
+        self._split(int(train * self.n), int((train + valid) * self.n), self.n)
+
+        self.scale = torch.from_numpy(self.scale).float()
+        tmp = self.test[1] * self.scale.expand(self.test[1].size(0), self.m)
+
+        self.scale = self.scale.to(device)
+        self.scale = Variable(self.scale)
+
+        self.rse = normal_std(tmp)
+        self.rae = torch.mean(torch.abs(tmp - torch.mean(tmp)))
+
+        self.device = device
+
+    def _normalized(self, normalize):
+        if(normalize == 0):
+            self.dat = self.rawdat
+
+        if(normalize == 1):
+            self.dat = self.rawdat / np.max(self.rawdat)
+        if(normalize == 2):
+            for i in range(self.m):
+                self.scale[i] = np.max(np.abs(self.rawdat[:, i]))
+                self.dat[:, i] = self.rawdat[:, i] / np.max(np.abs(self.rawdat[:,i]))
